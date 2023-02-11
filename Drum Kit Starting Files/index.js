b719c371e6c3a9ee8db4f33a984f3ed2da150eaa
joywin2003 <21a20.joywin@sjec.ac.in>
2023-02-11 22:56:57 +0530

    Added more functionalities

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,49 +1,63 @@
-
 var numberOfDrums = document.querySelectorAll(".drum").length
 
-for(var i=0;i<numberOfDrums;i++){
-  document.querySelectorAll("button")[i].addEventListener("click",function(){
+for (var i = 0; i < numberOfDrums; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
     var ButtonInnerHtml = this.innerHTML;
-    console.log(ButtonInnerHtml);
-    switch(ButtonInnerHtml){
-      case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        console.log(ButtonInnerHtml);
+    makeSound(ButtonInnerHtml);
+    buttonAnimation(ButtonInnerHtml);
+  });
+}
+
+document.addEventListener("keypress", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(choice) {
+  switch (choice) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
       break;
 
-      case "a":
-        var audio = new Audio("sounds/tom-2.mp3");
-        audio.play();
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
       break;
 
-      case "s":
-        var audio = new Audio("sounds/tom-3.mp3");
-        audio.play();
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
       break;
 
-      case "d":
-        var audio = new Audio("sounds/tom-4.mp3");
-        audio.play();
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
       break;
 
-      case "j":
-        var audio = new Audio("sounds/crash.mp3");
-        audio.play();
+    case "j":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
       break;
 
-      case "k":
-        var audio = new Audio("sounds/kick-bass.mp3");
-        audio.play();
+    case "k":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
       break;
 
-      case "l":
-        var audio = new Audio("sounds/snare.mp3");
-        audio.play();
+    case "l":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
       break;
-      default:console.log(ButtonInnerHtml);
-    }
-
-
-    });
+    default:
+      console.log(choice);
   }
+}
+
+function buttonAnimation(currentkey){
+  var activeKey = document.querySelector("."+currentkey);
+  activeKey.classList.add("pressed");
+  setTimeout(function(){
+    activeKey.classList.remove("pressed");
+  },100);
+}
